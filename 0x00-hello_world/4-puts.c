180e5bfb463c816bfbdfecaae8cc4113b4717408
godswillA1 <godswilllighteddy@gmail.com>
2023-02-16 10:00:08 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
