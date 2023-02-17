4e93667a79c611149ffe0018008b99da73d096ca
godswillA1 <godswilllighteddy@gmail.com>
2023-02-17 10:14:19 -0800

    Positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n)
 	else if (n < o)
-		printf("%d is negative\n". n)
+		printf("%d is negative\n", n)
 	else
 		printf("%d is zero\n", n)
 	return (0);
