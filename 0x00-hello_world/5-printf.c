0fe2ef0f0a478c66ef7da1b80235aaeebec675f9
godswillA1 <godswilllighteddy@gmail.com>
2023-02-16 16:44:51 -0800

    Use function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
- * Return: 0 (Success)
+ * main - print c program using printf
+ *
+ * Return: 0
  */
-
 int main(void)
 {
-	Printf("with proper grammar, but the outcome is a piece of art,\n")
+	Printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
