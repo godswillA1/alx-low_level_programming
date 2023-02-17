1ca46188999251096ae8b3161edc5e6e7493b769
godswillA1 <godswilllighteddy@gmail.com>
2023-02-16 17:31:06 -0800

    Territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various types on the computer it is compiled and run on
- * Return 0 (Success)
+ * main - print out sizes of data types in c
+ * code by Gwise techhub
+ * Return: 0
  */
-
 int main(void)
+
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
+	int a;
+	long int b;
+	long long int c;
+	char d;
 	float f;
 
-printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(b));
-printf("Size of long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-printf("Size of long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
-return(0)
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printnf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
