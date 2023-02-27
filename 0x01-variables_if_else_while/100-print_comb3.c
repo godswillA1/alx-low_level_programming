416e5d67ca8b60c87f55b845e4e8e0ebec0fb4cb
godswillA1 <godswilllighteddy@gmail.com>
2023-02-27 03:14:35 -0800

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+	int d;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+			if (d != c && d < c)
+				putchar('0' + d);
+				putchar('0' + c);
+				if (c + d != 17)
+				{
+					putchar(' ');
+					putchar(' ');
+				}
+	}
+	c++;
+
+}
+d++;
+}
+putchar('\n');
+return (0);
+}
