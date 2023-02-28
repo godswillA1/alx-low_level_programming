3b17499f3f11b66d55f272e1bfa98e154721c364
godswillA1 <godswilllighteddy@gmail.com>
2023-02-27 19:00:26 -0800

    Task 1 on nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,29 @@
 #include "main.h"
+#include <stdio.h>
+#include <unistd.h>
 
+/**
+ * print_alphabet - print alphabet
+ * Description: print alphabet in lowercase
+ * Return: has no return value
+ */
 
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
+void print_alphabet(void);
 
+/**
+ * print_alphabet - prints the alphabet
+ * Description: prints the alphabet in lowercase
+ * Return: has no return value
+ */
 
-	void print_alphabet(void)
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
 	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
+		_putchar(letter);
+		letter++;
 	}
+	_putchar('\n');
+}
